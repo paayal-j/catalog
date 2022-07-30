5fd5f46559f3e44b591616e86d9943a866326deb
paayal-j <payal.jaiin1612@gmail.com>
2022-07-30 10:50:34 +0530

    day2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_codepur/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,13 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    int days = 20;
+    String name = "Payal";
     return MaterialApp(
-        home: Material(
-      child: Center(
-        child: Container(
-          child: Text("Welcome Payal"),
-        ),
-      ),
-    ));
+      home: HomePage(),
+    );
   }
 }
