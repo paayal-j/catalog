e4475a80b83dab3e1c4a74b778dcca9296243fdd
paayal-j <payal.jaiin1612@gmail.com>
2022-08-06 17:53:10 +0530

    day17 completed

--- lib/books/catalog.dart
+++ lib/books/catalog.dart
@@ -2,6 +2,12 @@ import 'dart:convert';
 
 class CatalogBook {
   static List<Item> items = [];
+
+  //Get item by ID
+  static Item getById(int id) =>
+      items.firstWhere((element) => element.id == id, orElse: null);
+
+  static Item getByPosition(int pos) => items[pos];
 }
 
 class Item {
