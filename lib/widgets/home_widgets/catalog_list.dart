fafc37b00a35c4c0d2919ea0833a0b8ed0a0b157
paayal-j <payal.jaiin1612@gmail.com>
2022-08-07 21:17:14 +0530

    day17 completed

--- lib/widgets/home_widgets/catalog_list.dart
+++ lib/widgets/home_widgets/catalog_list.dart
@@ -17,7 +17,7 @@ class CatalogList extends StatelessWidget {
       shrinkWrap: true,
       itemCount: CatalogBook.items.length,
       itemBuilder: (context, index) {
-        final catalog = CatalogBook.items[index];
+        final catalog = CatalogBook.getByPosition(index);
         return InkWell(
           onTap: () => Navigator.push(
               context,
